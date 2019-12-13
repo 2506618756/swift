228bb278d217e49c1d21e02010f21f3a29e1146f
晨曦 <2506618756@qq.com>
2019-12-13 08:22:45 +0800

    Initial Commit

---
+++ 骰子/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  骰子
+//
+//  Created by S20181105302 on 2019/12/13.
+//  Copyright © 2019 s20181105302. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ 骰子Tests/__Tests.swift
@@ -0,0 +1,34 @@
+//
+//  __Tests.swift
+//  骰子Tests
+//
+//  Created by S20181105302 on 2019/12/13.
+//  Copyright © 2019 s20181105302. All rights reserved.
+//
+
+import XCTest
+@testable import __
+
+class __Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
