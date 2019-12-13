984d5c789cde33d2c125b3be56d912213adbb6fd
晨曦 <2506618756@qq.com>
2019-12-13 09:45:23 +0800

    swift骰子实验

--- 骰子/ViewController.swift
+++ 骰子/ViewController.swift
@@ -14,7 +14,17 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBOutlet weak var touzinoe: UIImageView!
+    
+    @IBOutlet weak var touzitwo: UIImageView!
+    
+    
+    @IBAction func yaoyiyao(_ sender: UIButton) {
+        var randomone = Int.random(in: 1...6)
+        var randomtwo = Int.random(in: 1...6)
+        touzinoe.image = UIImage(named: "dice\(randomone)")
+        touzitwo.image = UIImage(named: "dice\(randomtwo)")
+    }
 }
 
